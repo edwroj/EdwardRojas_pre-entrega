125462e35c43a1b79cec02fbe9b64c8e1e0bd331
edwroj <94715974+edwroj@users.noreply.github.com>
2025-05-13 22:26:52 -0300

    Add files via upload

---
+++ Pedido.java
@@ -0,0 +1,42 @@
+//clase base pedido
+public class Pedido {
+    private int id;
+    private String cliente;
+    private double monto;
+//constructor de pedido
+public Pedido(int id, String cliente, double monto) {
+    this.id = id;
+    this.cliente = cliente;
+    this.monto = monto;   
+
+}   
+// Getter para el atributo id (solo lectura)
+    public int getId() {
+        return id;
+    }
+    // no tengo un setter para el id, 
+// Getter para el atributo nombre
+    public String getcliente() {
+        return cliente;
+    }
+
+    // Setter para modificar el cliente
+    public void setcliente(String clienteParametro) {
+        this.cliente = clienteParametro;
+    }    
+    // Getter para el atributo monto no deberia poder modificar
+    
+    // Setter para modificar el monto no deberia poder modificar
+    
+
+    // Método para mostrar la información del artículo
+    public void mostrarpedido() {
+        // Este método puede ser sobrescrito por subclases (polimorfismo)
+        System.out.println("ID: " + id + " | Cliente: " + cliente + " | Monto: $" + monto);
+    }
+
+
+
+
+    
+}
